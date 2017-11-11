f2b815b98822f37b65693d8f98b1bee44f8e526c
Anton CHERNYSH <achernys@e1r4p1.unit.ua>
2017-11-11 03:04:38 +0200

    part 2

--- libft00/ft_strsplit.c
+++ libft00/ft_strsplit.c
@@ -6,11 +6,21 @@
 /*   By: achernys <achernys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:10:21 by achernys          #+#    #+#             */
-/*   Updated: 2017/11/09 20:06:46 by achernys         ###   ########.fr       */
+/*   Updated: 2017/11/09 17:10:40 by achernys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	**memfree(char **s)
+{
+	while (s != NULL)
+	{
+		ft_memdel((void **)s);
+		s++;
+	}
+	return (NULL);
+}
 
 static char	**strcreate(char const *s, char c, int tmp, int i)
 {
@@ -32,7 +42,7 @@ static char	**strcreate(char const *s, char c, int tmp, int i)
 		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
 		{
 			if (!(str[j++] = (char *)ft_memalloc(sizeof(char) * (tmp + 1))))
-				return (NULL);
+				return (memfree(str));
 			tmp = 0;
 		}
 		s++;
@@ -43,26 +53,26 @@ static char	**strcreate(char const *s, char c, int tmp, int i)
 
 char		**ft_strsplit(char const *s, char c)
 {
+	int		i;
 	int		j;
-	int		k;
 	char	**outsarr;
 
 	if (s == NULL || !(outsarr = strcreate(s, c, 0, -1)))
 		return (NULL);
+	i = 0;
 	j = 0;
-	k = 0;
 	while (*s != '\0')
 	{
 		if (*s != c)
 		{
-			outsarr[j][k] = *s;
-			k++;
+			outsarr[i][j] = *s;
+			j++;
 		}
 		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
 		{
-			outsarr[j][k] = '\0';
-			k = 0;
-			j++;
+			outsarr[i][j] = '\0';
+			j = 0;
+			i++;
 		}
 		s++;
 	}
